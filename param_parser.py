935b67b94ed9fa55fa1216cc8fe63b833e5d3aea
xueqicheng <xueqi.cheng@vanderbilt.edu>
2024-12-09 20:37:59 -0600

    update

--- param_parser.py
+++ param_parser.py
@@ -20,12 +20,12 @@ def parse_args():
     parser.add_argument('--n_out', type=int, default=32)
     parser.add_argument('--training', type=bool, default=True)
     parser.add_argument('--dropout', type=float, default=0.3)
-    parser.add_argument('--wd', type=float, default=0.0005)
+    parser.add_argument('--wd', type=float, default=0.0005, help='weight decay')
     parser.add_argument('--seed', type=int, default=1028)
     parser.add_argument('--runs', type=int, default=3)
 
     parser.add_argument('--method', type=str, default='tq', 
-                        help='choose from [none, qw, tw, tq]')
+                        help='choose from [none, qw, tq], qw: quantity reweight, tq: topological weight ') 
     
     parser.add_argument('--n', type=int, default=1, help='number of hop')
     parser.add_argument('--T', type=float, default=0.5, help='temperature of reweight')
@@ -33,7 +33,7 @@ def parse_args():
     
     parser.add_argument('--mixup', type=int, default=2, help='0: none, 1: topo k fix-mixup, 2: topo k random mixup')
     parser.add_argument('--alpha', type=float, default=4.0, help = 'mixup alpha hyperparamter')
-    parser.add_argument('--k', type=float, default=0, help='topo k hyperparamter in mixup')
+    parser.add_argument('--k', type=float, default=0, help='topo k percentage edges for mixup')
     parser.add_argument('--h', type=float, default=0, help = 'mixup loss factor')
     
     parser.add_argument('--beta', type=float, default=0.95, help = 'multi-hop ratio weight decay factor')
